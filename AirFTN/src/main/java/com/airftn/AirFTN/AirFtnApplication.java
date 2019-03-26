b74fdbdf8ec8a8c2fc7df7e2acb97a4e7db827f9
Srbislav <srbislav@tuta.io>
2019-03-26 11:42:31 +0100

    Creating application environment
    Setting up entire application

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/AirFtnApplication.java
@@ -0,0 +1,13 @@
+package com.airftn.AirFTN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFtnApplication.class, args);
+	}
+
+}
