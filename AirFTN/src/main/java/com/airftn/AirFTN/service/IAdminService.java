e926c1e9545076ab23805dc9d86360f286f91417
Srbislav <srbislav@tuta.io>
2019-03-28 00:25:27 +0100

    Admin service interface created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/service/IAdminService.java
@@ -0,0 +1,17 @@
+package com.airftn.AirFTN.service;
+
+import java.util.List;
+
+import com.airftn.AirFTN.model.Admin;
+
+public interface IAdminService {
+
+	List<Admin> findAll();
+	
+	Admin create(Admin administrator);
+	
+	Admin update(Admin administrator);
+	
+	boolean delete(Long id);
+	
+}
