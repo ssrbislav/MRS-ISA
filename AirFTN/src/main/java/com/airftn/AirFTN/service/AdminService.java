862bf7d6cb7f70fe7ce6f4f335c36675c02fa667
Srbislav <srbislav@tuta.io>
2019-03-28 00:58:39 +0100

    IAdminService implemented

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/service/AdminService.java
@@ -0,0 +1,45 @@
+package com.airftn.AirFTN.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.airftn.AirFTN.model.Admin;
+import com.airftn.AirFTN.repository.AdminRepository;
+
+public class AdminService implements IAdminService {
+
+	@Autowired
+	AdminRepository adminRepository;
+	
+	@Override
+	public List<Admin> findAll() {
+		return adminRepository.findAll(); 
+	}
+
+	@Override
+	public Admin create(Admin administrator) {
+		
+		//Only one administrator, so no need to cheek if one already exists
+		return adminRepository.save(administrator);
+	}
+
+	@Override
+	public Admin update(Admin administrator) {
+		
+		return adminRepository.save(administrator);
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		
+		for(Admin admin: findAll()) {
+			if(admin.getId() == id) {
+				adminRepository.delete(admin);
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
