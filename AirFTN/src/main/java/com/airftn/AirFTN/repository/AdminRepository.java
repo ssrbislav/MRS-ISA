029037c7a10cf2798342136e14ff83dc0ea70148
Srbislav <srbislav@tuta.io>
2019-03-27 01:39:58 +0100

    Admin Repository created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.airftn.AirFTN.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.airftn.AirFTN.model.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
