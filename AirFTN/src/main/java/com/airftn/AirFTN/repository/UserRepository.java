3d3ea8ca485094faa528fc927fac162f1c20192f
Srbislav <srbislav@tuta.io>
2019-03-27 01:38:32 +0100

    User Repository created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.airftn.AirFTN.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.airftn.AirFTN.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+	Optional<User> findById(Long id);
+	
+}
