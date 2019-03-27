a14e95a19c4030855f51689a33006813f41a423e
Srbislav <srbislav@tuta.io>
2019-03-28 00:35:30 +0100

    Passenger service interface created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/service/IPassengerService.java
@@ -0,0 +1,21 @@
+package com.airftn.AirFTN.service;
+
+import java.util.List;
+
+import com.airftn.AirFTN.model.Passenger;
+
+public interface IPassengerService {
+	
+	List<Passenger> findAll();
+	
+	Passenger getOne(Long ig);
+	
+	Passenger create(Passenger passenger);
+
+	Passenger update(Passenger passenger);
+	
+	boolean delete(Long id);
+	
+	boolean activate(Long id);
+	
+}
