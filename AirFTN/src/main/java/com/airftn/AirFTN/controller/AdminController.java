8061fbbb008e6008c1063f3209c01cdf7a58bf41
Srbislav <srbislav@tuta.io>
2019-03-28 02:41:37 +0100

    Delete method for Admin commented
    No need for delete method, only one sys Admin in the system

--- AirFTN/src/main/java/com/airftn/AirFTN/controller/AdminController.java
+++ AirFTN/src/main/java/com/airftn/AirFTN/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +32,9 @@ public class AdminController {
 	public ResponseEntity<Admin> create(@RequestBody Admin admin) {
 
 		Admin administrator = adminService.create(admin);
+		
+		if(administrator == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
 		return new ResponseEntity<>(administrator, HttpStatus.OK);
 	}
@@ -45,12 +47,15 @@ public class AdminController {
 		return new ResponseEntity<>(administrator, HttpStatus.OK);
 	}
 
+	//For now, only one administrator is allowed to exist in the system, so no delete method is needed
+	/*
 	@RequestMapping(value = "deleteAdmin/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Boolean> delete(@PathVariable Long id) {
 
 		boolean delete = adminService.delete(id);
-
+		
 		return new ResponseEntity<>(delete, HttpStatus.OK);
 	}
+	*/
 
 }
