795b8e25bdc89f37dfefc7a4914ef8ce0fc5828c
Srbislav <srbislav@tuta.io>
2019-03-29 23:46:19 +0100

    RegisterDTO created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/dto/RegisterDTO.java
@@ -0,0 +1,112 @@
+package com.airftn.AirFTN.dto;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class RegisterDTO {
+	
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private String username;
+
+	private Set<String> role;
+
+	@NotBlank
+	private String password;
+
+	@NotBlank
+	private String first_name;
+
+	@NotBlank
+	private String last_name;
+
+	@NotBlank
+	private String address;
+
+	@NotBlank
+	private String phone_number;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+	private Date date_birth;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Set<String> getRole() {
+		return role;
+	}
+
+	public void setRole(Set<String> role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public Date getDate_birth() {
+		return date_birth;
+	}
+
+	public void setDate_birth(Date date_birth) {
+		this.date_birth = date_birth;
+	}
+
+	
+	
+}
