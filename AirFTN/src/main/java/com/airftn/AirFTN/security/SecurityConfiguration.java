34c1a5fe3183a7f68ed066615094cd718fb1f0a5
Srbislav <srbislav@tuta.io>
2019-03-31 17:28:49 +0200

    Implementing security - first part

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/security/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.airftn.AirFTN.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.airftn.AirFTN.service.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest();
+
+	}
+}
+
--- AirFTN/src/main/java/com/airftn/AirFTN/service/ApplicationLoader.java
+++ AirFTN/src/main/java/com/airftn/AirFTN/service/ApplicationLoader.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.airftn.AirFTN.enumeration.RoleType;
@@ -26,32 +27,34 @@ public class ApplicationLoader implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		
-		
+
+
 		//Create System Admin if no admin is present
 		if (adminRepository.findAll().size() == 0) {
+			
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-					Role admin_role = new Role();
-					admin_role.setName(RoleType.ROLE_SYSADMIN);
-					Set<Role> roles = new HashSet<>();
-					roles.add(admin_role);
-					
-					Admin admin = new SysAdmin();
-					
-					admin.setUsername("sysadmin");
-					admin.setPassword("lozinka");
-					admin.setEmail("");
-					admin.setFirst_name("");
-					admin.setLast_name("");
-					admin.setAddress("");
-					admin.setDate_of_birth(new Date());
-					admin.setPhone_number("");
-					admin.setRoles(roles);
-	
-					adminRepository.save(admin);
-					System.out.println("Admin created!");	
+			Role admin_role = new Role();
+			admin_role.setName(RoleType.ROLE_SYSADMIN);
+			Set<Role> roles = new HashSet<>();
+			roles.add(admin_role);
+
+			Admin admin = new SysAdmin();
+
+			admin.setUsername("admin");
+			admin.setPassword(encoder.encode("admin"));
+			admin.setEmail("");
+			admin.setFirst_name("");
+			admin.setLast_name("");
+			admin.setAddress("");
+			admin.setDate_of_birth(new Date());
+			admin.setPhone_number("");
+			admin.setRoles(roles);
+
+			adminRepository.save(admin);
+			System.out.println("Admin created!");	
 		}
-	
+
 	}
 
 }
