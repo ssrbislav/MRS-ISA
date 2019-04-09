1c23b7e898d880495d6f5f898e75c6bfb5997cba
Srbislav <srbislav@tuta.io>
2019-04-09 14:02:42 +0200

    Login method created

--- AirFTN/src/main/java/com/airftn/AirFTN/controller/UserController.java
+++ AirFTN/src/main/java/com/airftn/AirFTN/controller/UserController.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.airftn.AirFTN.dto.LoginDTO;
 import com.airftn.AirFTN.dto.RegisterDTO;
 import com.airftn.AirFTN.enumeration.RoleType;
 import com.airftn.AirFTN.model.Passenger;
@@ -79,5 +84,24 @@ public class UserController {
 		return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
 
 	}
-
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginRequest) {
+		
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), 
+						loginRequest.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		String jwt = jwtProvider.generateJwtToken(authentication);
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+	
+		return new ResponseEntity<>(HttpStatus.OK);
+		
+		
+	}
+	
 }
+
