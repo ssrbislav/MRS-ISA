b4236faad5d06c1875de0780e3c5215c2d68be6a
Srbislav <srbislav@tuta.io>
2019-04-10 19:01:14 +0200

    Login implemented in ts

--- AirFTN-front/src/app/login/login.component.ts
+++ AirFTN-front/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginInfo } from '../auth/login-info';
 import { TokenStorageService } from '../auth/token-storage.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,17 @@ import { TokenStorageService } from '../auth/token-storage.service';
 })
 export class LoginComponent implements OnInit {
 
-  form: any = {}
+  form: any = {};
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
   private loginInfo: LoginInfo;
 
-  constructor(private tokenStorage: TokenStorageService) { }
+  constructor(private data: any,
+              private tokenStorage: TokenStorageService,
+              private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit() {
     if (this.tokenStorage.getToken() !== null) {
@@ -25,6 +30,43 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  onSubmit() {  }
+  onSubmit() {
+
+    this.loginInfo = new LoginInfo(
+      this.form.username,
+      this.form.password);
+
+    this.authService.attemptAuth(this.loginInfo).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+        this.isLoggedIn = true;
+        this.isLoginFailed = false;
+        this.roles = this.tokenStorage.getAuthorities();
+        this.roles.every(role => {
+          if (role === 'ROLE_SYS_ADMIN') {
+            this.router.navigate(['sysAdmin']);
+            return true;
+          } else if (role === 'ROLE_AIRLINE_ADMIN') {
+            this.router.navigate(['airlineAdmin']);
+            return true;
+          } else {
+            this.router.navigate(['passenger']);
+            return true;
+          }
+        });
+      },
+      error => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  relodePage() {
+    window.location.reload();
+  }
 
 }
