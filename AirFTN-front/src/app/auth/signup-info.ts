52212cd6ca05858ea90a9c6aaa2e237fe6080561
Srbislav <srbislav@tuta.io>
2019-04-10 17:01:58 +0200

    File to pass an object when user attemp to log in

---
+++ AirFTN-front/src/app/auth/signup-info.ts
@@ -0,0 +1,24 @@
+export class SignupInfo {
+
+    email: string;
+    username: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    address: string;
+    phone_number: string;
+    date_of_birth: string;
+
+    constructor(email: string, username: string, password: string, firstName: string, lastName: string,
+                address: string, phoneNumber: string, dateOfBrirth: string) {
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.first_name = firstName;
+            this.last_name = lastName;
+            this.address = address;
+            this.phone_number = phoneNumber;
+            this.date_of_birth = dateOfBrirth;
+    }
+
+}
