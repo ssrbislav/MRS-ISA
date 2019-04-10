8237ea0c43cb4fc760f0535cd1106e930f5120d0
Srbislav <srbislav@tuta.io>
2019-04-10 17:27:09 +0200

    Preventing user access if not logged in

--- AirFTN-front/src/app/app-routing.module.ts
+++ AirFTN-front/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
         component: MainPageComponent
     },
     {
+        path: 'mainPage',
+        component: MainPageComponent
+    },
+    {
         path: 'signup',
         component: RegistrationComponent
     },
