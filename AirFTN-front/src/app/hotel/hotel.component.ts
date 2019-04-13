6810164ad57d083f31e3b9d210bce88342f1b7f7
ssrbislav <srbislav@tuta.io>
2019-04-13 16:16:23 +0200

    Navigation correction on hotel page

--- AirFTN-front/src/app/hotel/hotel.component.ts
+++ AirFTN-front/src/app/hotel/hotel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-hotel',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit() {
+  }
+
+  main_navigate() {
+    this.location.back();
   }
 
 }
