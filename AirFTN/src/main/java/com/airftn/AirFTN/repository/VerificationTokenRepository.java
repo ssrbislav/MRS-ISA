cd44280929ca5f7b2b3ad151941e6f6a6cc07580
ssrbislav <srbislav@tuta.io>
2019-04-13 21:09:54 +0200

    VerificationToken repository created

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/repository/VerificationTokenRepository.java
@@ -0,0 +1,13 @@
+package com.airftn.AirFTN.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.airftn.AirFTN.model.User;
+import com.airftn.AirFTN.model.VerificationToken;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+	VerificationToken findByToken(String token);
+
+	VerificationToken findByUser(User user);
+}
--- AirFTN/src/main/java/com/airftn/AirFTN/service/UserDetailsServiceImpl.java
+++ AirFTN/src/main/java/com/airftn/AirFTN/service/UserDetailsServiceImpl.java
@@ -66,4 +66,5 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserService 
 		return null;
 	}
 
+
 }
