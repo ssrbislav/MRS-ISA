71a0d462ce38468c7ee745982b93c297cd7ae386
ssrbislav <srbislav@tuta.io>
2019-05-10 01:30:11 +0200

    Some refactoring

--- AirFTN-front/src/app/sys-admin/register-airadmin/register-airadmin.component.ts
+++ AirFTN-front/src/app/sys-admin/register-airadmin/register-airadmin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AirlineAdminDTO } from 'src/app/model/airlineAdmin.model';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
+import { Router } from '@angular/router';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-register-airadmin',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterAiradminComponent implements OnInit {
 
-  constructor() { }
+  errorMessage = '';
+  adminInfo: AirlineAdminDTO = new AirlineAdminDTO();
+  isSignedUp = false;
+
+  constructor(private tokenStorage: TokenStorageService,
+              private router: Router,
+              private adminService: AdminService) { }
 
   ngOnInit() {
+  }
+
+  cancelForm() {
+    this.router.navigate(['sysAdmin']);
+  }
+
+  onSubmit() {
+    this.adminService.adminRegistration(this.adminInfo).subscribe(
+      data => {
+        window.alert('Admin successfully registered!');
+        this.isSignedUp = true;
+        this.router.navigate(['sysAdmin']);
+      },
+      error => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+      }
+    );
   }
 
 }
