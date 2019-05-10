9e00c4c8602f1e691886eb7f5da4cc45773ddcbc
ssrbislav <srbislav@tuta.io>
2019-05-10 03:29:59 +0200

    Create AdminRespository.java

---
+++ AirFTN/src/main/java/com/airftn/AirFTN/repository/AdminRespository.java
@@ -0,0 +1,10 @@
+package com.airftn.AirFTN.repository;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRespository extends JpaRepository<Admin, Long> {
+
+	Admin findByUsername(String username);
+
+}
